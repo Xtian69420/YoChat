e808dd1128f97c7267efaf802cd031f4bc19f305
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2025-07-15 23:54:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: '*', 
-  credentials: true,
+  origin: '*'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
